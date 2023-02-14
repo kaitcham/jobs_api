8c49e4b52f1f73b528ee45db43acc0b5e4abd20f
Munezero Christian <chamsf5@aol.com>
2023-02-14 15:40:41 +0200

    Custom error handler middleware

---
+++ src/middlewares/errorHandler.js
@@ -0,0 +1,13 @@
+const { CustomAPIError } = require('../errors');
+const { StatusCodes } = require('http-status-codes');
+
+const errorHandlerMiddleware = (err, req, res, next) => {
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ message: err.message });
+  }
+  return res
+    .status(StatusCodes.INTERNAL_SERVER_ERROR)
+    .json({ message: err.message });
+};
+
+module.exports = errorHandlerMiddleware;
