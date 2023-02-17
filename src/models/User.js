ae950491a2b085270dca63e564e3178723b2ecc1
Munezero Christian <chamsf5@aol.com>
2023-02-17 23:09:04 +0200

    Create user schema

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    minlength: [3, 'Name must be at least 3 characters long'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      'Please provide a valid email address',
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters long'],
+  },
+});
+
+module.exports = mongoose.model('User', userSchema);
