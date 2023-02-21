209b04027aa1c7a25e00551226daf8bcda6f9aa9
Munezero Christian <chamsf5@aol.com>
2023-02-21 21:42:14 +0200

    Add logic that allows user to login in the system

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body });
@@ -8,7 +9,26 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    // return res
+    //   .status(StatusCodes.BAD_REQUEST)
+    //   .json({ message: 'Email and password are required' });
+    throw new BadRequestError('Email and password are required');
+  }
+
+  const checkUser = await User.findOne({ email });
+  if (!checkUser) {
+    throw new UnauthenticatedError('Invalid email');
+  }
+
+  const isMatch = await checkUser.checkPassword(password);
+  if (!isMatch) {
+    throw new UnauthenticatedError('Invalid password');
+  }
+
+  const formattedUser = checkUser.formatUserResponse();
+  res.status(StatusCodes.OK).json({ user: formattedUser });
 };
 
 module.exports = {
