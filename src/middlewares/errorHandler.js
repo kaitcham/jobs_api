d9faffd246718902c06133a6838636b853f7da78
Munezero Christian <chamsf5@aol.com>
2023-02-24 08:31:37 +0200

    Add condition to handle cast error

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -5,6 +5,15 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || 'Something went wrong',
   };
+
+  // Cast error
+  if (err.name === 'CastError') {
+    return res.status(StatusCodes.BAD_REQUEST).json({
+      message: `Resource with id: ${err.value} not found`,
+    });
+  }
+
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res
     .status(customError.statusCode)
     .json({ message: customError.message });
