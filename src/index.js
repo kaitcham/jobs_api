b4c86e371cb816b7725008b6533861724f0a4850
Munezero Christian <chamsf5@aol.com>
2023-02-28 22:39:35 +0200

    Add swagger documentation

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const express = require('express');
 const connectDB = require('./db/connect');
 const rateLimiter = require('express-rate-limit');
 
+const YAML = require('yamljs');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 
@@ -24,8 +28,15 @@ app.use(express.json());
 app.set('trust proxy', 1);
 app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100 }));
 
+app.get('/', (req, res) => {
+  res.send(
+    '<h1>Please navigate to docs </h1><br><a href="/api-docs">API Docs</a>'
+  );
+});
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use(notFoundError);
 app.use(customErrorHandler);
